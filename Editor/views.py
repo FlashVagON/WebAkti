32abb05d87b49c5ad34f4eedddd943b659359193
FlashVagON <djvagon@mail.ru>
2018-05-29 08:24:19 +0500

    добавил постраничный вывод
    Добавил страницы. но блять не работет эта х..ня. Взгляни где закосячил.

--- Editor/views.py
+++ Editor/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, Http404
 from django.contrib.auth.models import User
 from django.template import loader
@@ -26,12 +27,21 @@ def index(request):
 
 
 def detail(request, id):
-    try:
-        akt = T_NormativAct.objects.get(pk=id)
-    except T_NormativAct.DoesNotExist:
-        raise Http404("акт does not exist")
-    return render(request, 'Editor/detail.html', {'akt': akt})
-
+	try: akt = T_NormativAct.objects.get(pk=id)
+	except T_NormativAct.DoesNotExist:
+		raise Http404("акт does not	exist") 
+	return render(request, 'Editor/detail.html', {'akt': akt})
+	
+def listing(request, all_akts):							#https://djbook.ru/rel1.9/topics/pagination.html
+	akts_count = T_NormativAct.objects.all() #общее количество актов
+	paginator = Paginator(akts_count, 5)  # Показывать по 5 актов на странице
+	page = request.GET.get('page')
+	try: all_akts = paginator.page(page)
+	except PageNotAnInteger:  # If page is not an integer, deliver first page.
+		all_akts = paginator.page(1) 
+	except EmptyPage:  # If page is out of range (e.g. 9999), deliver last page of results. 
+		all_akts = paginator.page(paginator.num_pages)
+	return render_to_response(request, 'Editor/detail.html', {'all_akts':all_akts})
 
 class PostsListView(ListView):  # представление в виде списка
     model = T_NormativAct  # модель для представления
